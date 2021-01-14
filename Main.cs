68eded53a34d433285a7ee659b13b3082229dd3a
bmclean <mclean.ben@gmail.com>
2021-01-13 19:51:00 -0600

    first try

--- Main.cs
+++ Main.cs
@@ -5,6 +5,7 @@ public class Main : Node2D
 {
     public override void _Ready()
     {
-
+        Node midiPlayer = GD.Load<PackedScene>("res://addons/midi/MidiPlayer.tscn").Instance();
+        AddChild(midiPlayer);
     }
 }
