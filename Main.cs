89837aa85b625d4d6b89a6e92f645411819cf670
bmclean <mclean.ben@gmail.com>
2021-01-16 10:08:34 -0600

    Breaking it in the attempt to play MIDI data from byte array

--- Main.cs
+++ Main.cs
@@ -7,8 +7,11 @@ public class Main : Node2D
     {
         Node midiPlayer = (Node)GD.Load<GDScript>("res://addons/midi/MidiPlayer.gd").New();
         AddChild(midiPlayer);
-        midiPlayer.Set("file", "Music-133.mid");
         midiPlayer.Set("soundfont", "res://1mgm.sf2");
+        //midiPlayer.Set("file", "Music-133.mid");
+        Godot.Object smf = (Godot.Object)GD.Load<GDScript>("res://addons/midi/SMF.gd").New();
+        smf.Call("read_data", System.IO.File.ReadAllBytes("Music-133.mid"));
+        midiPlayer.Set("smf_data", smf);
         midiPlayer.Call("play", 0f);
     }
 }
